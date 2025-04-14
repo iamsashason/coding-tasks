36fa2633c81bd0c8584312dc528a98cc790d8c58
Sasha Son <svshvson@icloud.com>
2025-04-14 12:42:03 +0500

    Add template folder

---
+++ tasks/task_template/solution.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3 # Shebang line to specify the interpreter for executing the script.
+
+def main(): # Main function to execute the program.
+    print(function_name(arguments)) # Expected result
+
+if __name__ == "__main__": # Checks if the script is being run directly (not imported as a module).
+    main() # If the script is run directly (not imported as a module), calls the main function to execute the program.
